7c09c0e5a7920f6b666315086516464b83081bc7
rushi444 <rushi44@gmail.com>
2019-05-22 11:27:41 -0700

    contact and footer done

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,30 +1,3 @@
-// jQuery(document).ready(function () {
-//     "use strict";
-//     $('#slider-carousel').carouFredSel({
-//         responsive: true,
-//         width: '100%;',
-//         circular: true, 
-//         scroll: {
-//             items: 1, 
-//             duration: 500,
-//             pauseOnHover: true
-//         },
-//         auto: true,
-//         items: {
-//             visible: {
-//                 min: 1,
-//                 max: 1,
-//             },
-//             height: 'variable'
-//         },
-//         pagination: {
-//             container: '.sliderpager',
-//             pageAnchorbuilder: false 
-//         }
-        
-//     }); 
-
-
 
 $(window).scroll(function() { 
     let top = $(window).scrollTop(); 
