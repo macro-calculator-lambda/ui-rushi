0b03a90e315c3e6d608df73e5e4662ac5ef8949f
rushi444 <rushi44@gmail.com>
2019-05-22 16:35:56 -0700

    almost done except for one bug

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,40 @@
 
-$(window).scroll(function() { 
-    let top = $(window).scrollTop(); 
-    if(top >= 60) {
+$(window).scroll(function () {
+    let top = $(window).scrollTop();
+    if (top >= 60) {
         $('header').addClass('secondary');
     }
-    else if($('header').hasClass('secondary')){
+    else if ($('header').hasClass('secondary')) {
         ($('header').removeClass('secondary'))
-    } 
+    }
 
 })
 
- 
+
+//  carousel
+
+let carousel = document.querySelectorAll('.slider-carousel li');
+carousel = Array.from(carousel)
+console.log(carousel);
+
+function switchCarousel() {
+    // Hides all elements
+    carousel.forEach(function (element, index) {
+        element.classList.add('slider-content')
+    });
+
+    // Shows only 1
+    const randomElementIndex = Math.floor(Math.random() * carousel.length);
+    carousel[randomElementIndex].classList.remove('slider-content');
+}
+
+switchCarousel();
+
+setInterval(function() {
+    switchCarousel();
+}, 1000 * 5);
+
+
+
+
 
